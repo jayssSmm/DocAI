8986cb5ad8e4d7af2383c69c71e69d72a93bd1f3
Samman jaiswal <214768065+jayssSmm@users.noreply.github.com>
2026-03-08 16:25:12 +0000

    now you can  talk to ai with enabled memmory and a range of ai to choose from!

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask,render_template,request
-import api
+from api import groq_provider
 
 app=Flask(__name__)
+
+message=[]
 
 @app.route('/', methods=['GET','POST'])
 def index():
     if request.method=='POST':
+
         prompt=request.form.get('prompt')
-        response=api.response(prompt)
+        response=groq_provider.response(prompt,message)
+
+        message.append({'role':'assistant','content':response})
+
         return render_template('index.html',data=response)
 
     return render_template('index.html')
