321def839a43c69e7caea1c323eb3f6c60c78e05
Samman jaiswal <214768065+jayssSmm@users.noreply.github.com>
2026-03-12 16:21:12 +0000

    now pdf-handler has caching using redis

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask,render_template,request,session,redirect
 from api import groq_provider
 from api import gemini_provider
+import redis
 
 app=Flask(__name__)
 app.secret_key = "new2_random_string_here"
+
+r = redis.Redis(
+    host="localhost",
+    port=6379,
+    decode_responses=True
+)
 
 @app.route('/', methods=['GET','POST'])
 def index():
@@ -17,13 +24,25 @@ def index():
     if request.method=='POST':
         prompt=request.form.get('prompt')
         model=request.form.get('model')
+
+        cache=r.get(prompt)
+
         try:
             if model=='Groq':
-                response=groq_provider.response(prompt,session['chat_history_groq'])
-                session['chat_history_groq'].append({'role':'assistant','content':response})
-            else:
-                response=gemini_provider.response(prompt,session['chat_history_gemini'])
-                session['chat_history_gemini'].append({'role':'model','parts':[response]})
+                if cache:
+                    response=cache
+                else:
+                    response=groq_provider.response(prompt,session['chat_history_groq'])
+                    r.set(prompt,response,ex=300)
+                    session['chat_history_groq'].append({'role':'assistant','content':response})
+                    
+            elif model=='Gemini':
+                if cache:
+                    response=cache
+                else:
+                    response=gemini_provider.response(prompt,session['chat_history_gemini'])
+                    r.set(prompt,response,ex=300)
+                    session['chat_history_gemini'].append({'role':'model','parts':[response]})
 
             session.modified=True
 
