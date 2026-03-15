a9c845188e2d0f21a1fe5b9491c44031e6f5ceef
Samman jaiswal <214768065+jayssSmm@users.noreply.github.com>
2026-03-15 06:42:52 +0000

    removed gemini

--- static/upload.js
+++ static/upload.js
@@ -11,3 +11,38 @@ window.addEventListener('pageshow',()=>{
     submitBtn.disabled=false
 })
 
+
+const dropZone=document.getElementById('drop-zone')
+const dropBox=document.getElementById('drop-box')
+
+dropZone.addEventListener('dragover', (e)=>{
+    e.preventDefault()
+})
+
+dropZone.addEventListener('drop', (e)=>{
+    const dropData=e.dataTransfer.files()
+    dataHandler(dropData)
+})
+
+dropZone.addEventListener('click', (e)=>{
+    dropBox.click()
+})
+
+dropBox.addEventListener('change', (e)=>{
+    dataHandler(dropBox.files)
+})
+
+dropBox.addEventListener('paste', (e)=>{
+    const pasteData=e.clipboardData.items()
+    dataHandler(pasteData)
+})
+
+function dataHandler(data){
+    const formData = new FormData()
+    formData.append("data", data)
+
+    fetch("/upload", {
+    method: "POST",
+    body: formData
+    })
+}
