1a33e1093f4375016e980a9f2a629e78f165f20d
Samman jaiswal <214768065+jayssSmm@users.noreply.github.com>
2026-04-05 12:52:47 +0000

    added wsgi

--- app/routes/prompt.py
+++ app/routes/prompt.py
@@ -1,5 +1,5 @@
 from flask import Blueprint,request
-from app.services.cache_services import redis_text
+from app.services.llm_cache_services import redis_text
 from app.history import redis_history
 from app.services.yt_services import transcript_extractor
 from app.services.llm_services import groq_provider
