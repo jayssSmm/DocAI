b486de092a7b041c99145298379b951e7e7e44d7
Samman jaiswal <214768065+jayssSmm@users.noreply.github.com>
2026-04-10 15:28:11 +0000

    moved SECRET_KEY to .env

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = "ss@//42"
+    app.secret_key = os.getenv("SECRET_KEY")
 
     app.register_blueprint(main_bp)
     app.register_blueprint(ping_bp)
