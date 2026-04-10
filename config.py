39a72caa5d6af306e8289e8097007760eaf46cfe
Samman jaiswal <214768065+jayssSmm@users.noreply.github.com>
2026-04-10 14:48:53 +0000

    changed accesslog and errorlog in config.py to '-'

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ graceful_timeout=30
 max_request=1000
 max_request_jitter=50
 
-accesslog='gunicorn_access.log'
-errorlog='gunicorn_error.log'
+accesslog='-'
+errorlog='-'
 loglevel='info'
 
 bind = f"0.0.0.0:{os.environ.get('PORT', 10000)}"
